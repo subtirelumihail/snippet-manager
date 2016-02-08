458e1ee395c7e5b8a5ab393cecb8069c45562da7
Subtirelu Mihail <dazzzz2007@gmail.com>
2016-02-08 13:06:54 +0200

    Update routes

--- src/js/routes.js
+++ src/js/routes.js
@@ -4,12 +4,13 @@ import { Route, IndexRoute } from 'react-router';
 import App   from 'containers/App';
 
 import Snippet from 'components/snippet';
+import View    from 'components/view';
 
 export default  (
   <Route path="/" component={App}>
     <IndexRoute component={Snippet}/>
-    <Route path="/new" component={Snippet} />
-    <Route path="/list" component={App} />
-    <Route path="*" component={Snippet} />
+    <Route path="new" component={Snippet} />
+    <Route path=":url" component={View} />
+    <Route path="list" component={App} />
   </Route>
 );
